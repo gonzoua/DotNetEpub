c25bb417c6abdeef07d1fed3b5861b6413242092
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2011-07-24 19:33:06 -0700

    Initial code drop

---
+++ EPUBTest/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EPUBTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(49, 37);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(174, 23);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Generate ePUB";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 77);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
+            this.Text = "EPUBTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
